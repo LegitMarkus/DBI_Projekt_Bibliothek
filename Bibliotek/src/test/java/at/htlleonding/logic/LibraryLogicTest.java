0d036047a961d91a89d74f9c3679da6853f384e1
Markus Schwarz <herr-m-schwarz@gmx.at>
2022-04-22 15:41:23 +0200

    added TODO

--- Bibliotek/src/test/java/at/htlleonding/logic/LibraryLogicTest.java
+++ Bibliotek/src/test/java/at/htlleonding/logic/LibraryLogicTest.java
@@ -1,104 +1,69 @@
 package at.htlleonding.logic;
 
+import at.htlleonding.dto.AuthorDto;
+import at.htlleonding.repository.CRUDOperations;
 import io.quarkus.test.TestTransaction;
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @QuarkusTest
 class LibraryLogicTest {
-/*
+    /* VON MOODLE
     @Inject
-    LibraryLogic target;
+    LibraryMgmtLogic target;
 
-    private final String[][] authors = {
-            new String[] {"Manfred", "Spitzer", "27.05.1958", ""},
-            new String[] {"George", "Orwell", "25.06.1903", "21.01.1950"},
-            new String[] {"Winston", "Smith", "25.06.1903", "21.01.1945"}
-    };
-
-    private final String[][] books = {
-            new String[]{"Spitzer", "Lernen", "Nonfiction"},
-            new String[]{"Spitzer", "Musik im Kopf", "Nonfiction"},
-            new String[]{"Orwell", "1984", "Fiction", "Smith"},
-            new String[]{"Orwell", "Animal Farm", "Fiction"}
-    };
-
-    private final String[][] topics = {
-            new String[]{"Spitzer", "Lernen", "Lernpsychologie", "Psychologie", "Populärwissenschaft"},
-            new String[]{"Spitzer", "Musik im Kopf", "Lernpsychologie", "Musik"},
-            new String[]{"Orwell", "1984", "Dystopie", "Prophezeiung"},
-            new String[]{"Orwell", "Animal Farm", "Dystopie", "Biographie"}
-    };
+    @Inject
+    LibraryMgmtRepository repository;
 
     @TestTransaction
     @Test
-    void importLibraryData_3Authors_4Books() {
-        target.importLibraryData(authors, books, topics);
+    public void authorDTO_addUsingLogic_entityPersisted() {
+        var authorDTO = new AuthorDTO();
+        authorDTO.setDateBirth(LocalDate.of(1980, 1, 1));
+        authorDTO.setDateDeath(LocalDate.of(2000, 1, 1));
+        authorDTO.setFirstName("John");
+        authorDTO.setLastName("Doe");
 
-        var resultAuthors = target.getAuthors();
-        assertNotNull(resultAuthors);
-        assertEquals(3, resultAuthors.length);
+        var result = target.addAuthor(authorDTO);
+        Assertions.assertNotEquals(0, result);
 
-        var resultBooks = target.getBooks();
-        assertNotNull(resultBooks);
-        assertEquals(4, resultBooks.length);
+        target.flushAndClear();
+        var authorDB = repository.getAuthor(result);
+        Assertions.assertNotNull(authorDB);
+        Assertions.assertEquals(authorDTO.getLastName(), authorDB.getLastName());
     }
+    */
+    @Inject
+    LibraryMgmtLogic target;
+
+    @Inject
+    CRUDOperations repository;
 
     @TestTransaction
     @Test
-    void importLibraryDataSecondTime_SameCounters() {
-        target.importLibraryData(authors, books, topics);
+    public void authorDTO_addUsingLogic_entityPersisted() {
+        var authorDTO = new AuthorDto();
+        //authorDTO.setDateBirth(LocalDate.of(1980, 1, 1));
+        //authorDTO.setDateDeath(LocalDate.of(2000, 1, 1));
+        authorDTO.setFirstname("John");
+        authorDTO.setLastname("Doe");
 
-        var resultAuthors = target.getAuthors();
-        assertNotNull(resultAuthors);
-        assertEquals(3, resultAuthors.length);
+        //TODO Noch ned implementiert in Logic Implementieren
+        //var result = target.add(authorDTO);
+        //Assertions.assertNotEquals(0, result);
 
-        var resultBooks = target.getBooks();
-        assertNotNull(resultBooks);
-        assertEquals(4, resultBooks.length);
-
+        //target.flushAndClear();
+        //var authorDB = repository.getAuthor(result);
+        //Assertions.assertNotNull(authorDB);
+        //Assertions.assertEquals(authorDTO.getLastname(), authorDB.getLastName());
     }
-
-    @TestTransaction
-    @Test
-    void queryTopicDystopie_returns_1984_AnimalFarm() {
-        target.importLibraryData(authors, books, topics);
-
-        var result = target.getBooksOfTopic("Dystopie");
-
-        assertNotNull(result);
-        assertEquals(2, result.length);
-        assertEquals("Orwell", result[0][0]);
-        assertEquals("Orwell", result[1][0]);
-    }
-
-    @TestTransaction
-    @Test
-    void queryGenreNonfiction_returns_Spitzer() {
-        target.importLibraryData(authors, books, topics);
-
-        var result = target.getBooksOfGenre("Nonfiction");
-
-        assertNotNull(result);
-        assertEquals(2, result.length);
-        assertEquals("Spitzer", result[0][0]);
-        assertEquals("Spitzer", result[1][0]);
-    }
-
-    //@TestTransaction
-    @Test
-    void queryInventory_returns_inventory() {
-        target.importLibraryData(authors, books, topics);
-
-        var result = target.getInventory();
-
-        assertNotNull(result);
-        assertEquals(4, result.length);
-    }
- */
+    //TODO Alle anderen ENTITIES in der Logic implementieren
 }
